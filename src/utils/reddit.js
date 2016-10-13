5d8922f083f1f21d9c7d253d1d62c979bc509de4
ad222kr <ad222kr@student.lnu.se>
2016-10-13 15:32:08 +0200

    change prototol to https for reddit fetch

--- src/utils/reddit.js
+++ src/utils/reddit.js
@@ -1,5 +1,5 @@
 export const fetchPosts = (subreddit) => {
-  return fetch(`http://www.reddit.com/r/${subreddit}.json`)
+  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
     .then(res => res.json())
     .then(res => res.data.children)
 
