a91b56808a4625f5f2dd22aedfcafe935ad0f24b
ad222kr <ad222kr@student.lnu.se>
2016-10-13 15:20:47 +0200

    env vars not working on heroku :S

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './styles/index.css';
 
+console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID)
+console.log(process.env.REACT_APP_GOOGLE_SECRET)
+console.log(process.env.REACT_APP_REDIRECT_URL)
+console.log(process.env.REACT_APP_GOOGLE_API_KEY)
+
 console.log(process.env.REACT_APP_SECRET)
 ReactDOM.render(
   <App />,
