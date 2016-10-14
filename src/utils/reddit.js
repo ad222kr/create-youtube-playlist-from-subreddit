f96c4ad7dc2a38a4750872bd538de0b7a03a4c09
ad222kr <ad222kr@student.lnu.se>
2016-10-14 16:22:44 +0200

    remove token n such stuf from url for more security. url could get sent and token gives full access to youtube-account

--- src/utils/reddit.js
+++ src/utils/reddit.js
@@ -1,7 +1,6 @@
 export const fetchPosts = (subreddit) => {
   return fetch(`https://www.reddit.com/r/${subreddit}.json`)
     .then(res => {
-      console.log(res.status)
       if (res.status !== 200) 
         throw new Error("Something went wrong, are you sure you did not mistype the subreddit name?")
       return res
